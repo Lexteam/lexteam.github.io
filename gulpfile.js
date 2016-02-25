ce4a401ae092295378acefc6c65d1686fcd8e3a0
Jamie Mansfield <dev@jamierocks.uk>
2016-02-25 18:20:04 +0000

    The big experimental colour change

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var shell = require('gulp-shell');
 
-gulp.task('watch', function () {
-    gulp.watch('violet/source/*.scss', ['build']);
+gulp.task('watch', ['build'], function () {
+    gulp.watch('violet/source/**/*.scss', ['build']);
 });
 
 gulp.task('scss', function () {
@@ -20,5 +20,9 @@ gulp.task('build', ['scss'], function () {
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./violet/dist'));
 });
+
+gulp.task('serve', ['watch'], shell.task([
+    'jekyll serve'
+]));
 
 gulp.task('default', ['build']);
