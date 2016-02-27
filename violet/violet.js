6cdb0bf0cb4872dae918175851b1fec4341bfb97
Jamie Mansfield <dev@jamierocks.uk>
2016-02-27 18:30:51 +0000

    Add update fucntion to CLI

--- violet/violet.js
+++ violet/violet.js
@@ -2,6 +2,7 @@
 
 var ncp = require('ncp').ncp;
 var path = require('path');
+var fs = require('fs');
 
 require('yargs')
     .usage('$0 <cmd> [args]')
@@ -9,18 +10,46 @@ require('yargs')
         alias: 'd',
         describe: 'Provide the directory to install Violet to'
     })
-    .command('install', 'Install violet', {}, function (argv) {
+    .command('install', 'Install Violet', {}, function (argv) {
         var directory = 'violet';
         if (argv.directory != null) {
             directory = argv.directory;
         }
 
-        ncp(path.resolve(__dirname, './source'), directory, function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            console.log('Installed Violet!');
-        });
+        install(directory);
+    })
+    .command('update', 'Update Violet', {}, function (argv) {
+        var directory = 'violet';
+        if (argv.directory != null) {
+            directory = argv.directory;
+        }
+
+        deleteFolderRecursive(directory);
+        install(directory);
     })
     .help('help')
     .argv;
+
+function install(directory) {
+    ncp(path.resolve(__dirname, './source'), directory, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Installed Violet!');
+    });
+}
+
+// thanks to http://www.geedew.com/remove-a-directory-that-is-not-empty-in-nodejs/
+function deleteFolderRecursive(path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
