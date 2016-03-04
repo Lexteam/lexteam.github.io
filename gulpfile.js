2884ca9d1f0e44839ac02941d7ac996cee9a07dd
Jamie Mansfield <dev@jamierocks.uk>
2016-03-04 18:10:48 +0000

    Start work on Indigo (Violet 2.0.0)

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,39 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var shell = require('gulp-shell');
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch('violet/source/**/*.scss', ['build']);
+gulp.task('watch', ['violet:build'], function () {
+    gulp.watch('violet/source/**/*.scss', ['violet:build']);
+    gulp.watch('indigo/src/**/*.*', ['indigo:build']);
 });
 
-gulp.task('scss', function () {
+gulp.task('violet:scss', function () {
     return gulp.src('./violet/source/violet.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./violet/dist'));
 });
 
-gulp.task('build', ['scss'], function () {
+gulp.task('indigo:scss', function () {
+    return gulp.src('./indigo/src/scss/indigo.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./indigo/dist'));
+});
+
+gulp.task('violet:build', ['violet:scss'], function () {
     return gulp.src('./violet/dist/violet.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./violet/dist'));
 });
 
+gulp.task('indigo:build', ['indigo:scss'], function () {
+    return gulp.src('./indigo/dist/indigo.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./indigo/dist'));
+});
+
 gulp.task('serve', ['watch'], shell.task([
     'jekyll serve'
 ]));
 
-gulp.task('default', ['build']);
+gulp.task('default', ['violet:build', 'indigo:build']);
