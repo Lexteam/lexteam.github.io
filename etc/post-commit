ee184fafb0ddac6d75c44bd4e3712fab2c6d3568
Jamie Mansfield <dev@jamierocks.uk>
2016-03-25 12:09:33 +0000

    Housekeeping for Violet

---
+++ etc/post-commit
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git subtree split -P violet -b violet
+git push violet violet:master
--- etc/travis-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git remote add violet https://LexBot:${GH_TOKEN}@github.com/Lexteam/Violet >/dev/null
-git pull -s subtree violet master >/dev/null
-git subtree split -P violet -b violet
-git push violet violet:master >/dev/null
