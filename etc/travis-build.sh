ea6013495ac7e12ac0fd6f2f02940864ef5e1e9b
Jamie Mansfield <dev@jamierocks.uk>
2016-03-25 11:49:49 +0000

    Use the correct remote

--- etc/travis-build.sh
+++ etc/travis-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git remote add origin https://LexBot:${GH_TOKEN}@github.com/Lexteam/Violet >/dev/null
+git remote add violet https://LexBot:${GH_TOKEN}@github.com/Lexteam/Violet >/dev/null
 git subtree split -P violet -b violet
 git push violet violet:master > /dev/null
